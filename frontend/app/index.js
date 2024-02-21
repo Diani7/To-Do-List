01cdf19bd1744d501c1779d1ceceaf6b33213f2f
DianaCtes <dimctes@gmail.com>
2024-02-20 22:34:20 -0500

    created logic for create task

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -5,4 +5,45 @@ class Task {
         this.date = date;
         this.state = state;
     }
+
+    getName(){
+        return this.name;
+    }
+
+    getDate(){
+        return this.date;
+    }
+
+    getState(){
+        return this.state;
+    }
+
+    setName(name){
+        this.name = name;
+        return `the task with name: ${this.name} was created`
+    }
 }
+
+function createTask() {
+    document.addEventListener("submit", (event) => {
+        event.preventDefault();
+        const taskName = document.getElementById("task_name");
+        //console.log(taskName.value);
+        const task = new Task(taskName.value);
+        console.log("name", task.getName(), "date", task.getDate(), "state", task.getState());
+    })
+}
+createTask()
+
+function validateEmptyField(){
+    const addTask = document.getElementById("add_task");
+    const taskName = document.getElementById("task_name");
+    console.log(taskName.value.length);
+    if (taskName.value.length > 0){
+        addTask.disabled = false;
+    } else {
+        addTask.disabled = true;
+    }
+}
+
+
